9f0e21d0bfe94303103c35d952f4a60aef6aabc3
djimenezchocron <djimenezweb@gmail.com>
2023-01-25 21:58:45 +0100

    Clases corregidas
    Falta simplificar función

--- src/js/submitword.js
+++ src/js/submitword.js
@@ -13,11 +13,14 @@ const submitWord = (word, secretWord, gameBoard) => {
   for (let index = 0; index < word.length; index++) {
     if (wordToCheck.includes(word[index])) {
       gameBoard.children[currentRow].children[index].textContent = word[index];
-      if (gameBoard.children[currentRow].children[index].classList.contains('game-board__box--correct')) return;
-      gameBoard.children[currentRow].children[index].classList.add('game-board__box--included');
+      if (!gameBoard.children[currentRow].children[index].classList.contains('game-board__box--correct')) {
+        gameBoard.children[currentRow].children[index].classList.add('game-board__box--included');
+      }
     } else {
       gameBoard.children[currentRow].children[index].textContent = word[index];
-      gameBoard.children[currentRow].children[index].classList.add('game-board__box--not-included');
+      if (!gameBoard.children[currentRow].children[index].classList.contains('game-board__box--correct')) {
+        gameBoard.children[currentRow].children[index].classList.add('game-board__box--not-included');
+      }
     }
   }
   currentRow++;
